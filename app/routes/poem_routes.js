244885ab8825ad1a1555d16f2cb8cc45fbb1e9e0
KLLANO23 <llano.kristian@gmail.com>
2021-04-27 14:39:38 -0400

    Work on poem_routes

--- app/routes/poem_routes.js
+++ app/routes/poem_routes.js
@@ -4,39 +4,48 @@ const Poem = require('./../models/poem')
 const handle404 = require('./../../lib/custom_errors')
 const passport = require('passport')
 const requireToken = passport.authenticate('bearer', {session: false})
+const removeBlanks = require('../../lib/remove_blank_fields')
+const customErrors = require('../../lib/custom_errors')
+const requireOwnership = customErrors.requireOwnership
 
 router.post('/poems', requireToken, (req, res, next) => {
-  const poemData = req.body.poem
-  poemData.user = req.user.id
-  Poem.create(poemData)
-    .then(poem => res.status(201).json({ poem }))
+  req.body.poem.owner = req.user.id
+  Poem.create(req.body.poem)
+    .then(poem => {
+      res.status(201).json({ poem: poem.toObject() })
+    })
     .catch(next)
 })
 
-router.get('/poems/:id', (req, res, next) => {
-  const id = req.params.id
-  Poem.findById(id)
-    .populate('owner')
-    .then(handle404)
-    .then(poem => res.json({poem}))
+router.get('/poems', requireToken, (req, res, next) => {
+  const id = req.user.id
+  Poem.find({ owner: id })
+    .then(poems => {
+      return poems.map(poem => poem.toObject())
+    })
+    .then(poems => res.status(200).json({ poems }))
     .catch(next)
 })
 
-router.patch('/poems/:id', requireToken, (req, res, next) => {
-  const id = req.params.id
-  const poemData = req.body.poem
-  Poem.findById(id)
+router.patch('/poems/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.poem.owner
+  Poem.findById(req.params.id)
     .then(handle404)
-    .then(poem => poem.updateOne(poemData))
+    .then(poem => {
+      requireOwnership(req, poem)
+      return poem.updateOne(req.body.poem)
+    })
     .then(() => res.sendStatus(204))
     .catch(next)
 })
 
 router.delete('/poems/:id', requireToken, (req, res, next) => {
-  const id = req.params.id
-  Poem.findById(id)
+  Poem.findById(req.params.id)
     .then(handle404)
-    .then(poem => poem.deleteOne())
+    .then(poem => {
+      requireOwnership(req, poem)
+      poem.deleteOne()
+    })
     .then(() => res.sendStatus(204))
     .catch(next)
 })
