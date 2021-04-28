6c3e07510de126a544ce534433282d5c183847cf
KLLANO23 <llano.kristian@gmail.com>
2021-04-28 16:26:39 -0400

    Create, update, delete working

--- app/routes/poem_routes.js
+++ app/routes/poem_routes.js
@@ -4,10 +4,12 @@ const Poem = require('../models/poem')
 const handle404 = require('./../../lib/custom_errors')
 const passport = require('passport')
 const requireToken = passport.authenticate('bearer', {session: false})
+const removeBlanks = require('../../lib/remove_blank_fields')
+const customErrors = require('../../lib/custom_errors')
+const requireOwnership = customErrors.requireOwnership
 
 router.post('/poems', requireToken, (req, res, next) => {
   req.body.poem.owner = req.user.id
-
   Poem.create(req.body.poem)
     .then(poem => {
       res.status(201).json({ poem: poem.toObject() })
@@ -15,30 +17,41 @@ router.post('/poems', requireToken, (req, res, next) => {
     .catch(next)
 })
 
-router.get('/poems/:id', (req, res, next) => {
-  const id = req.params.id
-  Poem.findById(id)
-    .populate('owner')
-    .then(handle404)
-    .then(poem => res.json({poem}))
+router.get('/poems', requireToken, (req, res, next) => {
+  const id = req.user.id
+  // find the poems with owner of req.user.id
+  Poem.find({ owner: id })
+    .then(poems => {
+      // `poems` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return poems.map(poem => poem.toObject())
+    })
+    // respond with status 200 and JSON of the poems
+    .then(poems => res.status(200).json({ poems: poems }))
+    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
-router.patch('/poems/:id', requireToken, (req, res, next) => {
-  const id = req.params.id
-  const poemData = req.body.poem
-  Poem.findById(id)
+router.patch('/poems/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.poem.owner
+  Poem.findById(req.params.id)
     .then(handle404)
-    .then(poem => poem.updateOne(poemData))
+    .then(poem => {
+      requireOwnership(req, poem)
+      return poem.updateOne(req.body.poem)
+    })
     .then(() => res.sendStatus(204))
     .catch(next)
 })
 
 router.delete('/poems/:id', requireToken, (req, res, next) => {
-  const id = req.params.id
-  Poem.findById(id)
+  Poem.findById(req.params.id)
     .then(handle404)
-    .then(poem => poem.deleteOne())
+    .then(poem => {
+      requireOwnership(req, poem)
+      poem.deleteOne()
+    })
     .then(() => res.sendStatus(204))
     .catch(next)
 })
--- curl-scripts/poems/index.sh
+++ curl-scripts/poems/index.sh
@@ -7,7 +7,7 @@ curl "${API}${URL_PATH}" \
   --include \
   --request GET \
   --header "Content-Type: application/json" \
-  --header "Authorization: Bearer ${TOKEN}"
+  --header "Authorization: Bearer ${TOKEN}" \
 
 echo
 
